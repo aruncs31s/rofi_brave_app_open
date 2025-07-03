8edfb4f2d451b97e0fbf263a1adfc20f629c8eb0
Arun CS <aruncs31ss@gmail.com>
2025-07-03 16:22:38 +0530

    initial model

---
+++ rofi_brave_app.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+# Date: 2025-07-03
+# Author: Arun CS
+
+
+
+declare -A urls
+urls[" Github"]="https://github.com"
+urls[" Youtube"]="https://youtube.com"
+urls[" Google"]="https://google.com"
+urls[" YT Music"]="https://music.youtube.com"
+
+options=$(printf "%s\n" "${!urls[@]}")
+
+selected_option=$(echo -e "$options" | rofi -dmenu -p "Open in Brave App")
+
+if [ -n "$selected_option" ]; then
+    url=${urls[$selected_option]}
+    brave --app="$url"
+fi
+
